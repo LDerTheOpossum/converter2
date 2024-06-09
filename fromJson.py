1daad9042640c864ba6b94adeda022fa0383f917
Krzysztof Koba <dsw51692@student.dsw.edu.pl>
2024-06-09 16:12:52 +0200

    preps4

---
+++ fromJson.py
@@ -0,0 +1,46 @@
+import sys
+import json
+
+def parse_args(args):
+    if len(args) != 3:
+        print("Usage: python main.py pathFile1.x pathFile2.y")
+        sys.exit(1)
+
+    input_file = args[1]
+    output_file = args[2]
+
+    return input_file, output_file
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        print("JSON data loaded successfully")
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Error loading JSON: {e}")
+        sys.exit(1)
+
+def save_json(data, file_path):
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print("JSON data saved successfully")
+    except Exception as e:
+        print(f"Error saving JSON: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    input_file, output_file = parse_args(sys.argv)
+    if input_file.endswith('.json'):
+        data = load_json(input_file)
+        if output_file.endswith('.json'):
+            save_json(data, output_file)
+        else:
+            print("Unsupported output file format")
+            sys.exit(1)
+    else:
+        print("Unsupported input file format")
+        sys.exit(1)
+    print(f"Input file: {input_file}")
+    print(f"Output file: {output_file}")
